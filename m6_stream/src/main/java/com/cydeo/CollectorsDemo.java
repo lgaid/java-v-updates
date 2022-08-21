8cc7de3205293d93af7584a68d7a395297c20099
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-21 17:30:53 -0400

    Collectors

--- m6_stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6_stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -1,7 +1,14 @@
 package com.cydeo;
 
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+import com.cydeo.task.DishTask;
+import com.cydeo.task.Type;
+
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.awt.SystemColor.menu;
 
 public class CollectorsDemo {
 
@@ -22,6 +29,57 @@ public class CollectorsDemo {
 
         System.out.println(numberSet);
 
+        //toList() : returns a Collector interface that gathers the input data into a new list
+        List<Integer> numberList2 = numbers.stream()
+                .filter(x -> x%2==0)
+                .collect(Collectors.toList());
+
+        //toSet() : returns a Collector interface that gathers the input data into a new set
+        Set<Integer> numberSet2 = numbers.stream()
+                .filter(x -> x%2==0)
+                .collect(Collectors.toSet());
+
+        //toMap(Function,Function) : returns a Collector interface that gathers the input data into a new map
+        Map<String,Integer> dishMap = DishData.getAll().stream()
+                .collect(Collectors.toMap(Dish::getName,Dish::getCalories));
+
+        System.out.println(dishMap);
+
+        //summingInt(ToIntFunction) : returns a Collector that produces the sum of a Integer-valued func
+        Integer sum = DishData.getAll().stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println(sum);
+
+        //counting() : returns a Collector that counts the number of the elements
+        Long evenCount = numbers.stream()
+                .filter(x -> x%2==0)
+                .collect(Collectors.counting());
+
+        System.out.println(evenCount);
+
+        // averagingInt(ToIntFunction): returns the average of the integers passed values
+        Double calorieAverage = DishData.getAll().stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+
+        //joining(): Is used to join various elements of a character or string array into a single object
+        List<String> courses = Arrays.asList("Java", "JS", "TS");
+        String str = courses.stream()
+                .collect(Collectors.joining(","));
+        System.out.println(str);
+
+        //partitioningBy(): is used to partition a steam of objects (or a set of elements) based on a given predicate
+        Map<Boolean,List<Dish>> veggieDish = DishData.getAll().stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+
+        System.out.println(veggieDish);
+
+        //groupingBy(): is used for grouping by some property and storing results in Map instance
+
+
+        Map<Type,List<Dish>> dishType = DishData.getAll().stream()
+                .collect(Collectors.groupingBy(Dish::getType));
+
+        System.out.println(dishType);
 
 
 
