0627a6f1548f6dafd5d660d00fd80901bc46bc3c
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-23 14:47:18 -0400

    textBlock

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -0,0 +1,29 @@
+package com.cydeo.java13;
+
+public class TextBlockDemo {
+
+
+    public static void main(String[] args) {
+
+
+        var address = "7925 Jones Branch Drive \n " + "Mclean,VA 22102";
+
+        System.out.println(address);
+
+        var address2 = """
+                7925 Jones Branch Drive
+                McLean, VA 22102
+                """;
+
+        System.out.println(address2);
+
+
+
+
+
+
+    }
+
+
+
+}
