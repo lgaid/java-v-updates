614271b74e7d13a5d7d329a5f0838cbab0a7c773
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-23 12:47:51 -0400

    var

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
@@ -0,0 +1,30 @@
+package com.cydeo.java10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LocalVariableTypeDemo {
+
+    public static void main(String[] args) {
+
+        // Type Inference *** interview question
+
+        var str = "Cydeo";
+        var number = 5;
+
+        byte x = 1;
+        var y = 1; // int
+
+        var price = 12.50;
+
+        var numbers = Arrays.asList(3,4,5,6);
+
+
+
+
+
+
+    }
+
+
+}
